0a89cf1739b7c40a9cedea44de71b4dfe8d3a1df
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-10-04 23:45:01 +0900

    修正2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,7 +11,7 @@ class OrdersController < ApplicationController
 
   def create
     ActiveRecord::Base.transaction do
-      @order = current_user.orders.build(order_params)
+      @order = current_user.lock.orders.build(order_params)
       @order.save!
       @order.update_total_quantity
     end
